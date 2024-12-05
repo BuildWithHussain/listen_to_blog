344cb17133b0084098aed65827bfda0cc15baf3c
Hussain Nagaria <hussainbhaitech@gmail.com>
2024-12-05 19:33:23 +0530

    feat: automatically generate and attach audio file

--- listen_to_blog/hooks.py
+++ listen_to_blog/hooks.py
@@ -137,13 +137,11 @@ app_license = "mit"
 # ---------------
 # Hook on document methods and events
 
-# doc_events = {
-# 	"*": {
-# 		"on_update": "method",
-# 		"on_cancel": "method",
-# 		"on_trash": "method"
-# 	}
-# }
+doc_events = {
+	"Blog Post": {
+		"on_update": "listen_to_blog.utils.generate_audio_file_for_blog_post",
+	}
+}
 
 # Scheduled Tasks
 # ---------------
