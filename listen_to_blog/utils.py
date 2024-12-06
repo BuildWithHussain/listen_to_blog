5b0dec627fc92751ed3ae4deacce1219371b97ee
Hussain Nagaria <hussainbhaitech@gmail.com>
2024-12-06 18:58:57 +0530

    chore: throw if no api key

--- listen_to_blog/utils.py
+++ listen_to_blog/utils.py
@@ -5,5 +5,8 @@ from elevenlabs.client import ElevenLabs
 def get_elevenlabs_client():
 	api_key = frappe.get_cached_doc("ElevenLabs Settings").get_password("api_key")
 
+	if not api_key:
+		frappe.throw("Please set your API Key in ElevenLabs Settings before generating audio!")
+
 	return ElevenLabs(api_key=api_key)
 
